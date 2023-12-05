6530e1e09f0b244ae658baff155ce8fdd3ec3aec
Kathoey <nicholasnguyen75@gmail.com>
2023-12-05 15:32:14 -0500

    updated app

---
+++ src/components/Results.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="block">
+            <p>Reaction time: {{ score }} ms</p>
+        </div>
+        
+        <p class="rank">Rank: {{ rank }}</p>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "Fast Fingers"
+        } else if (this.score < 400) {
+            this.rank = "Rapid Reflexes"
+        } else {
+            this.rank = "Slow Snail"
+        }
+    }
+}
+</script>
+
+<style>
+
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+        font-style: italic;
+    }
+</style>
